7c829191a229f8240d22df9fb876d4fd7a489a78
MateoRivadeneira14 <mateoriva98@gmail.com>
2024-11-12 13:07:57 -0500

    feat: :rocket: Create dockerfile
    Deploy and build the image with dockerfile

--- main.go
+++ main.go
@@ -7,19 +7,15 @@ import (
 )
 
 func main() {
-	// Ruta para servir archivos estáticos (como imágenes)
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./templates"))))
-
-	// Ruta para servir la página principal con el template
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		tmpl, err := template.ParseFiles("templates/index.html")
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+			http.Error(w, "Hola Mundo en GO", http.StatusInternalServerError)
 			return
 		}
 		tmpl.Execute(w, nil)
 	})
 
-	log.Println("Servidor en http://localhost:3002")
-	http.ListenAndServe(":3002", nil)
+	log.Println("Servidor en ejecución en http://localhost:3002")
+	log.Fatal(http.ListenAndServe(":3002", nil))
 }
