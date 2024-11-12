26bb37f7371ecc962e92b7617133e362349a1a49
MateoRivadeneira14 <mateoriva98@gmail.com>
2024-11-12 13:00:26 -0500

    feat: :zap: Add lines code
    Add lines code to improve the projects

--- main.go
+++ main.go
@@ -6,30 +6,20 @@ import (
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	// Parsear el archivo index.html desde la carpeta templates
-	tmpl, err := template.ParseFiles("templates/index.html")
-	if err != nil {
-		log.Println("Error al cargar el template:", err)
-		http.Error(w, "Error interno del servidor", http.StatusInternalServerError)
-		return
-	}
-
-	// Ejecutar el template y escribirlo en la respuesta HTTP
-	err = tmpl.Execute(w, nil)
-	if err != nil {
-		log.Println("Error al ejecutar el template:", err)
-		http.Error(w, "Error interno del servidor", http.StatusInternalServerError)
-	}
-}
-
 func main() {
-	// Servir archivos estáticos de la carpeta 'images'
-	http.Handle("/images/", http.StripPrefix("/images", http.FileServer(http.Dir("images"))))
+	// Ruta para servir archivos estáticos (como imágenes)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./templates"))))
 
-	// Ruta para la página principal
-	http.HandleFunc("/", handler)
+	// Ruta para servir la página principal con el template
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl, err := template.ParseFiles("templates/index.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		tmpl.Execute(w, nil)
+	})
 
-	log.Println("Servidor escuchando en puerto 8080...")
-	http.ListenAndServe(":8080", nil)
+	log.Println("Servidor en http://localhost:3002")
+	http.ListenAndServe(":3002", nil)
 }
